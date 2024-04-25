e0dfa99d4a34cb319644117b1442f3558451c392
tamara_danilova <roznica14@yandex.ru>
2024-04-25 16:33:33 +0400

    added test navbar

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,12 +1,9 @@
-import { Button } from '@/shared/ui/Button/Button';
 import styles from './MainPage.module.scss';
 
 export const MainPage = () => {
     return (
         <div className={styles.main}>
-            <Button>
-                В КАТАЛОГ
-            </Button>
+
         </div>
     );
 };
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,9 +1,22 @@
 import { memo } from "react";
+import banner from '@/shared/assets/images/banner.png';
+import styles from './Navbar.module.scss';
+import { classNames } from "@/shared/lib/classNames/classNames";
+import { Button } from "@/shared/ui/Button/Button";
 
 export const Navbar = memo(() => {
     return (
-        <nav>
-            Navbar
-        </nav>
+        <header>
+            <img
+                className={classNames(styles.images, {}, [])}
+                src={banner} alt="savannah"
+            />
+            <nav className={classNames(styles.navbar, {}, [])}>
+                NAVBAR
+                NAVBAR
+                NAVBAR
+            </nav>
+            <Button className={styles.button}>В КАТАЛОГ</Button>
+        </header>
     );
 });
