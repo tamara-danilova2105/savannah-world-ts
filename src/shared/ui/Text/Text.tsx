52d0d4946610c46ba65296185d5d90d70763c2f7
tamara-danilova2105 <sap1028@mail.ru>
2024-05-26 13:55:41 +0400

    added Text

---
+++ src/shared/ui/Text/Text.tsx
@@ -0,0 +1,45 @@
+import { ReactNode, memo } from "react";
+import styles from './Text.module.scss';
+import { classNames } from "@/shared/lib/classNames/classNames";
+
+export type TextTag = 'h1' | 'h2' | 'h3' | 'p' | 'li';
+export type TextSize = 'xs' | 's' | 'm' | 'l' | 'xl';
+
+interface TextProps {
+    className?: string ,
+    children: ReactNode,
+    tag?: TextTag,
+    size?: TextSize,
+}
+
+export const Text = memo((props: TextProps) => {
+    const {
+        className,
+        children,
+        tag = 'p',
+        size = 's',
+    } = props;
+
+    const mapTextTag: Record<TextTag, TextTag> = {
+        h1: 'h1',
+        h2: 'h2',
+        h3: 'h3',
+        p: 'p',
+        li: 'li'
+    };
+
+    const TextTag = mapTextTag[tag];
+
+    const additional = [
+        className,
+        styles[size],
+    ]
+
+    return (
+        <TextTag 
+            className={classNames(styles.text, {}, additional)}
+        >
+            {children}
+        </TextTag>
+    );
+});
