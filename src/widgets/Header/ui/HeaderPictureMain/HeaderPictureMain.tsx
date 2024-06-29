84b19d495cb2952788504e1594d1a478ef363bd8
tamara-danilova2105 <sap1028@mail.ru>
2024-06-29 20:50:41 +0400

    upd header mobile

--- src/widgets/Header/ui/HeaderPictureMain/HeaderPictureMain.tsx
+++ src/widgets/Header/ui/HeaderPictureMain/HeaderPictureMain.tsx
@@ -14,10 +14,6 @@ import picture_768 from "../../../../shared/assets/banner/banner_768x432.jpg";
 import picture_768_2x from "../../../../shared/assets/banner/banner_768x432@2x.jpg";
 import picture_768webp from "../../../../shared/assets/banner/banner_768x432.webp";
 import picture_768_2xwebp from "../../../../shared/assets/banner/banner_768x432@2x.webp";
-import picture_480 from "../../../../shared/assets/banner/banner_480x270.jpg";
-import picture_480_2x from "../../../../shared/assets/banner/banner_480x270@2x.jpg";
-import picture_480webp from "../../../../shared/assets/banner/banner_480x270.webp";
-import picture_480_2xwebp from "../../../../shared/assets/banner/banner_480x270@2x.webp";
 import picture_320 from "../../../../shared/assets/banner/banner_320x180.jpg";
 import picture_320_2x from "../../../../shared/assets/banner/banner_320x180@2x.jpg";
 import picture_320webp from "../../../../shared/assets/banner/banner_320x180.webp";
@@ -39,14 +35,10 @@ export const HeaderPictureMain = () => {
                 srcSet={`${picture_1024webp} 1x, ${picture_1024_2xwebp} 2x`} type="image/webp" />
             <source media="(min-width: 1024px)"
                 srcSet={`${picture_1024} 1x, ${picture_1024_2x} 2x`}/>
-            <source media="(min-width: 768px)"
+            <source media="(min-width: 821px)"
                 srcSet={`${picture_768webp} 1x, ${picture_768_2xwebp} 2x`} type="image/webp" />
-            <source media="(min-width: 768px)"
+            <source media="(min-width: 821px)"
                 srcSet={`${picture_768} 1x, ${picture_768_2x} 2x`}/>
-            <source media="(min-width: 480px)"
-                srcSet={`${picture_480webp} 1x, ${picture_480_2xwebp} 2x`} type="image/webp" />
-            <source media="(min-width: 480px)"
-                srcSet={`${picture_480} 1x, ${picture_480_2x} 2x`}/>
             <source media="(min-width: 320px)"
                 srcSet={`${picture_320webp} 1x, ${picture_320_2xwebp} 2x`} type="image/webp"/>
             <img className={styles.picture} src={picture_320}
