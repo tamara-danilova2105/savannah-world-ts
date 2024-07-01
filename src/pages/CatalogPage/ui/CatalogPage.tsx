390a6469632d4f47eab430871499094fff074f06
tamara_danilova <roznica14@yandex.ru>
2024-07-01 09:49:12 +0400

    upd

--- src/pages/CatalogPage/ui/CatalogPage.tsx
+++ src/pages/CatalogPage/ui/CatalogPage.tsx
@@ -1,10 +1,17 @@
 import { Filter } from "@/features/Fiiter";
-import styles from'./CatalogPage.module.scss';
+import styles from './CatalogPage.module.scss';
+import { BrowserView, MobileView } from "react-device-detect";
+import { filterIcon } from "@/shared/assets/svg/filterIcon";
 
 export const CatalogPage = () => {
     return (
         <main className={styles.main}>
-            {/* <Filter /> */}
+            <BrowserView>
+                <Filter />
+            </BrowserView>
+            <MobileView>
+                {filterIcon()}
+            </MobileView>
         </main>
     );
 };
