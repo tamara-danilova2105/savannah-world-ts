06498657e6c857524b637a4a2095e38e1b7c6211
tamara-danilova2105 <sap1028@mail.ru>
2024-07-01 20:58:31 +0400

    upd footer

--- src/widgets/Footer/ui/Footer.tsx
+++ src/widgets/Footer/ui/Footer.tsx
@@ -2,12 +2,14 @@ import { memo } from 'react';
 import styles from './Footer.module.scss';
 import { Stack } from '@/shared/ui/Stack/Stack';
 import { contacts } from '../lib/data';
+import { Text } from '@/shared/ui/Text/Text';
 
 export const Footer = memo(() => {
     return (
         <footer className={styles.footer}>
-            <Stack
+            <Stack 
                 justify='between' max
+                className={styles.container}
             >
                 <Stack
                     direction='column'
@@ -23,7 +25,12 @@ export const Footer = memo(() => {
                     )}
                 </Stack>
 
-                <Stack direction='column' gap='16' align='end'>
+                <Stack 
+                    direction='column' 
+                    gap='16' 
+                    align='end'
+                    className={styles.mediaDesktop}
+                >
                     {contacts.slice(3).map(contact =>
                         <Stack gap='16'>
                             {contact.text}
@@ -31,6 +38,27 @@ export const Footer = memo(() => {
                         </Stack>
                     )}
                 </Stack>
+                <Stack 
+                    max
+                    justify='center'
+                    className={styles.mediaMobile}
+                >
+                    {contacts.slice(3).map(contact =>
+                        <>{contact.icon}</>
+                    )}
+                </Stack>
+            </Stack>
+
+            <Stack 
+                justify='between' max
+                className={styles.info}
+            >
+                <Text className={styles.text}>
+                    2024 <span>Savannah World.</span> Все права защищены
+                </Text>
+                <Text className={styles.text}>
+                    Сайт сделан в <span>AL TECH LABS LTD.</span>
+                </Text>
             </Stack>
         </footer>
     );
