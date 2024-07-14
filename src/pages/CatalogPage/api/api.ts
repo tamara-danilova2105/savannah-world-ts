320c5f10862a3e17ca7a662131fe20ac6206c883
tamara-danilova2105 <sap1028@mail.ru>
2024-07-14 13:37:10 +0400

    added test filter getCats

--- src/pages/CatalogPage/api/api.ts
+++ src/pages/CatalogPage/api/api.ts
@@ -1,11 +1,23 @@
 import { api } from "@/shared/api/api";
+import { getQueryParams } from "@/shared/lib/getQueryParams/getQueryParams";
 
 const catsApi = api.injectEndpoints({
     endpoints: (build) => ({
         getCats: build.query({
-            query: () => ({
-                url: '/',
-            }),
+            query: (filterParams) => {
+                const params: Record<string, string> = {
+                    group: filterParams.group?.join(','),
+                    sex: filterParams.sex?.join(','),
+                    age: filterParams.age?.join(','),
+                    shipment: filterParams.shipment?.join(',')
+                };
+
+                const queryParams = getQueryParams(params);
+
+                return {
+                    url: `/${queryParams}`,
+                };
+            },
         }),
         saveCat: build.mutation({
             query: (newCats) => ({
