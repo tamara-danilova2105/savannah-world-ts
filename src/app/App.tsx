29eaba2f8e6e3661d3fcc9a21429e72f2fcb970d
tamara_danilova <roznica14@yandex.ru>
2024-07-15 10:28:52 +0400

    added

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,9 +2,9 @@ import { Suspense } from "react";
 import AppRouter from "./providers/router/ui/AppRouter";
 import { ButtonUp } from "@/shared/ui/ButtonUp/ButtonUp";
 import { ContactUs } from "@/features/ContactUs";
-import { NavbarMobile } from "@/features/Navbar";
+import { NavbarMobile } from "@/widgets/Navbar";
 import { Footer } from "@/widgets/Footer";
-import { Navbar } from "@/features/Navbar";
+import { Navbar } from "@/widgets/Navbar";
 
 function App() {
   return (
