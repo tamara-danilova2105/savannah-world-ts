4b24ec346f3251633cab0c8f8248c3897b3d5286
tamara-danilova2105 <sap1028@mail.ru>
2024-07-16 06:40:52 +0400

    fix public api slice

--- src/features/FormCatCard/index.ts
+++ src/features/FormCatCard/index.ts
@@ -1,5 +1,9 @@
 import { FormCatCard } from "./ui/FormCatCard/FormCatCard";
+import { getCatCard, resetCatCard, setCatCard } from './model/slices/slice';
 
 export {
     FormCatCard,
+    getCatCard,
+    resetCatCard,
+    setCatCard,
 };
